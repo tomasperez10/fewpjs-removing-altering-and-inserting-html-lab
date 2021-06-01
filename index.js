96704a8f65139ac703d3272f0e52aa87723154c7
Tomas Perez <tomasitopg6@gmail.com>
2021-06-01 07:47:51 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let main = document.getElementById('main')
+main.remove();
+
+var newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "is the champion";
